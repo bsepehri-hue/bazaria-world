309412e557303489307015ae2a44c82515f85e98
bsepehri-hue <bsepehri@gmail.com>
2026-01-31 18:09:28 -0800

    Update LandFilters.tsx

--- components/LandFilters.tsx
+++ components/LandFilters.tsx
@@ -1,3 +1,15 @@
-export default function LandFilters() {
-  return <div>Land Filters Placeholder</div>;
+"use client";
+
+interface LandFiltersProps {
+  onChange: (filters: any) => void;
 }
+
+export default function LandFilters({ onChange }: LandFiltersProps) {
+  return (
+    <div>
+      Land Filters Placeholder
+    </div>
+  );
+}
+
+
