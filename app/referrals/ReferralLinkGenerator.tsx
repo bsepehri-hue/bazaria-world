9f322934eda15220709c999308d073df1a2ebbb1
bsepehri-hue <bsepehri@gmail.com>
2026-01-31 19:11:59 -0800

    Create ReferralLinkGenerator.tsx

---
+++ app/referrals/ReferralLinkGenerator.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ReferralLinkGenerator() {
+  return (
+    <div className="p-4 border rounded-lg">
+      Referral Link Generator Placeholder
+    </div>
+  );
+}
