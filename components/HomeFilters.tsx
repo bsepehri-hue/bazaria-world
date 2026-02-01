deb7e51ca18862205747ee92b3ad12d6960bc506
bsepehri-hue <bsepehri@gmail.com>
2026-01-31 17:54:37 -0800

    Update HomeFilters.tsx

--- components/HomeFilters.tsx
+++ components/HomeFilters.tsx
@@ -1,3 +1,14 @@
-export default function HomeFilters() {
-  return <div>Home Filters Placeholder</div>;
+"use client";
+
+interface HomeFiltersProps {
+  onChange: (filters: any) => void;
 }
+
+export default function HomeFilters({ onChange }: HomeFiltersProps) {
+  return (
+    <div>
+      Home Filters Placeholder
+    </div>
+  );
+}
+
