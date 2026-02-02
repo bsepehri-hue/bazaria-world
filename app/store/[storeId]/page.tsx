6fd2f2bf7dcc2ca2a51d473142c32f2787d83038
bsepehri-hue <bsepehri@gmail.com>
2026-02-01 19:02:49 -0800

    Update page.tsx

--- app/store/[storeId]/page.tsx
+++ app/store/[storeId]/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import AuctionCard from "@/components/AuctionCard";
+import AuctionCard from "../../components/AuctionCard";
+
+
 
 type StorePageProps = {
   params: {
