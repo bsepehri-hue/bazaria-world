96fb1fb1e23388282d27015882d0a24248f95a09
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 23:54:10 -0800

    Update updateNotificationSettings.ts

--- actions/settings/updateNotificationSettings.ts
+++ actions/settings/updateNotificationSettings.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/server";
 import { doc, updateDoc } from "firebase/firestore";
 import { NotificationSettings } from "@/lib/settings/settings";
 import { SettingsFormState } from "@/lib/settings/types";
