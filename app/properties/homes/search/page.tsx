de1f6c9216a8caa92adbed909d6320c68fe6736c
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 22:30:41 -0800

    Update page.tsx

--- app/properties/homes/search/page.tsx
+++ app/properties/homes/search/page.tsx
@@ -9,7 +9,7 @@ import {
   getDocs,
   orderBy,
 } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/client";
 import HomeFilters from "@/components/HomeFilters";
 
 
