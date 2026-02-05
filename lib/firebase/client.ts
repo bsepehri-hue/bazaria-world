61822b46619e227b9d59aa5b17badf10c9191eb2
bsepehri-hue <bsepehri@gmail.com>
2026-02-05 00:17:22 -0800

    Update client.ts

--- lib/firebase/client.ts
+++ lib/firebase/client.ts
@@ -10,7 +10,8 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
 };
 
-const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const app =
+  getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
