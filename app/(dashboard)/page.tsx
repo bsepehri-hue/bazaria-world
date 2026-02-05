16c864e645802507a79b498a908924c6725a3955
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 21:24:52 -0800

    Update page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/client";
 import { collection, onSnapshot } from "firebase/firestore";
 import {
   Store,
