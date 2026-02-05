3ceb30f1a46a3157a5f10f14994ad313eabe76a3
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 23:22:48 -0800

    Update page.tsx

--- app/vehicles/trucks/[listingId]/page.tsx
+++ app/vehicles/trucks/[listingId]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/client";
 import { createPaymentIntent } from "@/lib/payments/createPaymentIntent";
 import { loadStripe } from "@stripe/stripe-js";
 import MessageButton from "@/components/MessageButton";
