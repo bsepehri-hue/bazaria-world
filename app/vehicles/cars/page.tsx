da0fa0fc1053a3aa58b24e30f1539e45fd4d4645
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 22:52:41 -0800

    Update page.tsx

--- app/vehicles/cars/page.tsx
+++ app/vehicles/cars/page.tsx
@@ -9,7 +9,7 @@ import {
   getDocs,
   orderBy,
 } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/client";
 import MessageButton from "@/components/MessageButton";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebase";
