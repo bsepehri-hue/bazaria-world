98ad99768bdb09c4a49511a75816a2da6bf7f9c9
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 19:57:58 -0800

    Update getAccountStatus.ts

--- lib/stripe/getAccountStatus.ts
+++ lib/stripe/getAccountStatus.ts
@@ -1,8 +1,9 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-11-01",
+  apiVersion: "2025-12-15.clover",
 });
+
 
 /**
  * Retrieve the status of a connected account.
