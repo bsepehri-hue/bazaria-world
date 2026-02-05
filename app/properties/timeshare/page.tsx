6bd59e4d71f5e31285b3a397454651b809e7f3f7
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 22:36:49 -0800

    Update page.tsx

--- app/properties/timeshare/page.tsx
+++ app/properties/timeshare/page.tsx
@@ -9,7 +9,7 @@ import {
   getDocs,
   orderBy,
 } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/client";
 
 export default function TimeshareCategoryPage() {
   const router = useRouter();
