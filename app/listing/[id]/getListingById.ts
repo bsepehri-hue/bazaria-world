b00ed94de5a1155b8327038ac647216b4aa81840
bsepehri-hue <bsepehri@gmail.com>
2026-02-04 20:47:09 -0800

    Update getListingById.ts

--- app/listing/[id]/getListingById.ts
+++ app/listing/[id]/getListingById.ts
@@ -1,4 +1,4 @@
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase/server";
 import { doc, getDoc } from "firebase/firestore";
 
 export async function getListingById(id: string) {
