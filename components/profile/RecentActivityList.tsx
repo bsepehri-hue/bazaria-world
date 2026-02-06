61bfeeeeed5bbee57b5472f2e18908ac111f7ecf
bsepehri-hue <bsepehri@gmail.com>
2026-02-05 21:17:34 -0800

    Update RecentActivityList.tsx

--- components/profile/RecentActivityList.tsx
+++ components/profile/RecentActivityList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { Zap, Clock } from 'lucide-react';
-import { mockRecentActivity, getActivityIcon } from '@/lib/mockData/profile';
+import { getActivityIcon } from '@/lib/mockData/profile';
 
 interface ActivityListProps {
   activities: any[];
