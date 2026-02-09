dbe1997c110983cc9de3f58e564fe2f39280dc27
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 22:01:31 -0800

    Update onUnpaidWin.ts

--- functions/   rewards/triggers/onUnpaidWin.ts
+++ functions/   rewards/triggers/onUnpaidWin.ts
@@ -1,37 +1,18 @@
-import * as functions from "firebase-functions";
 import { loadRewardsState } from "../utils/loadRewardsState";
 import { saveRewardsState } from "../utils/saveRewardsState";
 import { appendHistory } from "../utils/appendHistory";
 
 import { auctionEngine } from "../engines/auctionEngine";
 import { penaltyEngine } from "../engines/penaltyEngine";
-import { cooldownEngine } from "../engines/cooldownEngine";
-import { trustEngine } from "../engines/trustEngine";
-import { tierEngine } from "../engines/tierEngine";
 import { eligibilityEngine } from "../engines/eligibilityEngine";
 
-export const onUnpaidWin = functions.firestore
-  .document("auctions/{auctionId}/unpaid/{recordId}")
-  .onCreate(async (snap, context) => {
-    const record = snap.data();
-    if (!record) return;
+export const onUnpaidWin = async (userId: string, winId: string) => {
+  const state = await loadRewardsState(userId);
 
-    const userId = record.userId;
-    if (!userId) return;
+  auctionEngine.updateOnUnpaidWin(state);
+  penaltyEngine.applyPolicyStrike(state);
+  eligibilityEngine.evaluate(state);
 
-    const state = await loadRewardsState(userId);
-
-    auctionEngine.updateOnUnpaidWin(state);
-
-    penaltyEngine.apply(state, "disputeLoss");
-
-    cooldownEngine.apply(state, "bidding", 24);
-
-    trustEngine.recompute(state);
-    tierEngine.recompute(state);
-    eligibilityEngine.recompute(state);
-
-    await saveRewardsState(userId, state);
-
-    await appendHistory(userId, "unpaid_win", record.recordId || context.params.recordId);
-  });
+  await saveRewardsState(userId, state);
+  await appendHistory(userId, "UNPAID_WIN", winId);
+};
