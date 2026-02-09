e52dc3963e1e21444236138ff299571b74e189ae
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:33:02 -0800

    Update stewardEngine.ts

--- functions/   rewards/engines/stewardEngine.ts
+++ functions/   rewards/engines/stewardEngine.ts
@@ -1,1 +1,19 @@
+export const stewardEngine = {
+  updateVerification(state) {
+    state.steward.verificationsCompleted += 1;
+  },
 
+  updateDisputeMediation(state, accurate) {
+    state.steward.disputeMediations += 1;
+
+    if (accurate) {
+      state.steward.accuracyRate = 
+        (state.steward.accuracyRate * (state.steward.disputeMediations - 1) + 1) /
+        state.steward.disputeMediations;
+    }
+  },
+
+  addBonus(state, amount) {
+    state.steward.bonusesEarned += amount;
+  }
+};
