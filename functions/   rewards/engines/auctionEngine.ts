91fc5443f05958b13761b36cf7f519872e9791ab
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:32:20 -0800

    Update auctionEngine.ts

--- functions/   rewards/engines/auctionEngine.ts
+++ functions/   rewards/engines/auctionEngine.ts
@@ -1,1 +1,17 @@
+export const auctionEngine = {
+  updateOnBidPlaced(state) {
+    state.auctions.bidsPlaced += 1;
+  },
 
+  updateOnAuctionWon(state) {
+    state.auctions.bidsWon += 1;
+  },
+
+  updateOnUnpaidWin(state) {
+    state.auctions.unpaidWins += 1;
+  },
+
+  updateOnBidRetraction(state) {
+    state.auctions.bidRetractions += 1;
+  }
+};
