7dbf31f1bf27cf0f47a2e01ebcb36b8633ad16a6
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:30:42 -0800

    Update cooldownEngine.ts

--- functions/   rewards/engines/cooldownEngine.ts
+++ functions/   rewards/engines/cooldownEngine.ts
@@ -1,1 +1,26 @@
+import * as admin from "firebase-admin";
 
+export const cooldownEngine = {
+  apply(state, type, hours) {
+    const end = admin.firestore.Timestamp.fromMillis(
+      Date.now() + hours * 60 * 60 * 1000
+    );
+
+    switch (type) {
+      case "selling":
+        state.cooldowns.selling = end;
+        break;
+
+      case "bidding":
+        state.cooldowns.bidding = end;
+        break;
+
+      case "messaging":
+        state.cooldowns.messaging = end;
+        break;
+
+      default:
+        return;
+    }
+  }
+};
