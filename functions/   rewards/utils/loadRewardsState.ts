419211c510f9cf0cdbd4a12ac23b9f5a0285655d
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 21:45:59 -0800

    Update loadRewardsState.ts

--- functions/   rewards/utils/loadRewardsState.ts
+++ functions/   rewards/utils/loadRewardsState.ts
@@ -1,1 +1,19 @@
+import { db } from "../firebase";
+import { RewardsState } from "../state/RewardsState";
+import { defaultRewardsState } from "../state/defaultRewardsState";
 
+export const loadRewardsState = async (userId: string): Promise<RewardsState> => {
+  const ref = db.collection("rewardsState").doc(userId);
+  const snap = await ref.get();
+
+  if (!snap.exists) {
+    const fresh = defaultRewardsState(userId);
+    await ref.set(fresh);
+    return fresh;
+  }
+
+  const data = snap.data() as Partial<RewardsState>;
+  const merged = { ...defaultRewardsState(userId), ...data };
+
+  return merged;
+};
