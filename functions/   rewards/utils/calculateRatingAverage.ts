ecae6c9f713b7be38ea83010bcfdef96e003b85f
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:26:53 -0800

    Update calculateRatingAverage.ts

--- functions/   rewards/utils/calculateRatingAverage.ts
+++ functions/   rewards/utils/calculateRatingAverage.ts
@@ -1,1 +1,4 @@
-
+export const calculateRatingAverage = (currentAvg: number, count: number, newRating: number) => {
+  if (count === 0) return newRating;
+  return (currentAvg * count + newRating) / (count + 1);
+};
