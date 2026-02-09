af6c8e00dd0c5bf525fabdbc46877cc9cbc2feef
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 22:00:15 -0800

    Update onBidPlaced.ts

--- functions/   rewards/triggers/onBidPlaced.ts
+++ functions/   rewards/triggers/onBidPlaced.ts
@@ -1,31 +1,16 @@
-import * as functions from "firebase-functions";
 import { loadRewardsState } from "../utils/loadRewardsState";
 import { saveRewardsState } from "../utils/saveRewardsState";
 import { appendHistory } from "../utils/appendHistory";
 
 import { auctionEngine } from "../engines/auctionEngine";
-import { trustEngine } from "../engines/trustEngine";
-import { tierEngine } from "../engines/tierEngine";
 import { eligibilityEngine } from "../engines/eligibilityEngine";
 
-export const onBidPlaced = functions.firestore
-  .document("bids/{bidId}")
-  .onCreate(async (snap, context) => {
-    const bid = snap.data();
-    if (!bid) return;
+export const onBidPlaced = async (userId: string, bidId: string) => {
+  const state = await loadRewardsState(userId);
 
-    const bidderId = bid.userId;
-    if (!bidderId) return;
+  auctionEngine.updateOnBidPlaced(state);
+  eligibilityEngine.evaluate(state);
 
-    const state = await loadRewardsState(bidderId);
-
-    auctionEngine.updateOnBidPlaced(state);
-
-    trustEngine.recompute(state);
-    tierEngine.recompute(state);
-    eligibilityEngine.recompute(state);
-
-    await saveRewardsState(bidderId, state);
-
-    await appendHistory(bidderId, "bid_placed", bid.bidId || context.params.bidId);
-  });
+  await saveRewardsState(userId, state);
+  await appendHistory(userId, "BID_PLACED", bidId);
+};
