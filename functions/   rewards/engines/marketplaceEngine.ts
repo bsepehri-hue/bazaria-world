2bc87a1add2e22335a1ef1978c09674436745ead
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:31:37 -0800

    Update marketplaceEngine.ts

--- functions/   rewards/engines/marketplaceEngine.ts
+++ functions/   rewards/engines/marketplaceEngine.ts
@@ -1,1 +1,28 @@
+import { calculateDeliveryRate, calculateRatingAverage } from "../utils";
 
+export const marketplaceEngine = {
+  updateOnOrderCompleted(state, order) {
+    state.marketplace.itemsSold += 1;
+
+    if (order.deliveredOnTime) {
+      state.marketplace.onTimeDeliveryRate = calculateDeliveryRate(
+        state.marketplace.itemsSold, 
+        state.marketplace.itemsListed
+      );
+    }
+  },
+
+  updateOnRatingReceived(state, rating) {
+    state.marketplace.averageRating = calculateRatingAverage(
+      state.marketplace.averageRating,
+      state.marketplace.ratingCount,
+      rating
+    );
+
+    state.marketplace.ratingCount += 1;
+  },
+
+  updateOnCancellation(state) {
+    state.marketplace.cancellationRate += 1;
+  }
+};
