31b3a417a60503abe2b1d0dbfb69210c459d4fa1
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:30:22 -0800

    Update penaltyEngine.ts

--- functions/   rewards/engines/penaltyEngine.ts
+++ functions/   rewards/engines/penaltyEngine.ts
@@ -1,1 +1,24 @@
+export const penaltyEngine = {
+  apply(state, type) {
+    switch (type) {
+      case "lateShipment":
+        state.penalties.lateShipment += 1;
+        break;
 
+      case "cancellation":
+        state.penalties.cancellation += 1;
+        break;
+
+      case "disputeLoss":
+        state.penalties.disputeLoss += 1;
+        break;
+
+      case "policyStrike":
+        state.penalties.policyStrike += 1;
+        break;
+
+      default:
+        return;
+    }
+  }
+};
