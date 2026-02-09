13dea37f16a01ebf3f4f3dd49d21a18331015992
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 21:47:02 -0800

    Update appendHistory.ts

--- functions/   rewards/utils/appendHistory.ts
+++ functions/   rewards/utils/appendHistory.ts
@@ -1,1 +1,19 @@
+import { db } from "../firebase";
 
+export const appendHistory = async (
+  userId: string,
+  eventType: string,
+  referenceId: string
+) => {
+  const ref = db
+    .collection("rewardsHistory")
+    .doc(userId)
+    .collection("events")
+    .doc();
+
+  await ref.set({
+    eventType,
+    referenceId,
+    timestamp: new Date()
+  });
+};
