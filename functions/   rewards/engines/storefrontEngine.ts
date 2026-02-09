e87cab00cec077f47c1c01da58eb2e9acf47c7f9
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:31:58 -0800

    Update storefrontEngine.ts

--- functions/   rewards/engines/storefrontEngine.ts
+++ functions/   rewards/engines/storefrontEngine.ts
@@ -1,1 +1,15 @@
+export const storefrontEngine = {
+  updateOnOrderCompleted(state) {
+    state.storefront.ordersCompleted += 1;
+  },
 
+  updateOnDisputeResolved(state, outcome) {
+    if (outcome === "loss") {
+      state.storefront.disputes += 1;
+    }
+  },
+
+  updateRepeatCustomer(state) {
+    state.storefront.repeatCustomers += 1;
+  }
+};
