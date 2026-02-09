59d79c8fc9556bcdc61da2df5f598d0df5371811
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 20:32:37 -0800

    Update referralEngine.ts

--- functions/   rewards/engines/referralEngine.ts
+++ functions/   rewards/engines/referralEngine.ts
@@ -1,1 +1,10 @@
+export const referralEngine = {
+  updateOnReferralCompleted(state) {
+    state.referralStats.totalReferred += 1;
+  },
 
+  updateOnReferralActivated(state) {
+    state.referralStats.activeReferred += 1;
+    state.referralStats.bonusesEarned += 10;
+  }
+};
