552cd2c30c552162351dd24969c6604247b33e09
bsepehri-hue <bsepehri@gmail.com>
2026-02-08 21:45:29 -0800

    Update saveRewardsState.ts

--- functions/   rewards/utils/saveRewardsState.ts
+++ functions/   rewards/utils/saveRewardsState.ts
@@ -1,1 +1,7 @@
+import { db } from "../firebase";
+import { RewardsState } from "../state/RewardsState";
 
+export const saveRewardsState = async (userId: string, state: RewardsState) => {
+  const ref = db.collection("rewardsState").doc(userId);
+  await ref.set(state, { merge: true });
+};
