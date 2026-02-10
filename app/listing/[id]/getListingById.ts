a0c959d7dc0055a2736eb477713969c084227628
bsepehri-hue <bsepehri@gmail.com>
2026-02-09 23:06:52 -0800

    Delete app/listing/[id] directory

--- app/listing/[id]/getListingById.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "@/lib/firebase/server";
-import { doc, getDoc } from "firebase/firestore";
-
-export async function getListingById(id: string) {
-  const ref = doc(db, "listings", id);
-  const snap = await getDoc(ref);
-  return snap.exists() ? snap.data() : null;
-}
