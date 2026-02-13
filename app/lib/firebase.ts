c46345ac11acc109b7f48b0c752c668afafaabdd
bsepehri-hue <bsepehri@gmail.com>
2026-02-12 22:38:54 -0800

    Update firebase.ts

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,9 +1,8 @@
-
-
 // app/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,6 +15,7 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
+export const auth = getAuth(app);      // ⭐ THIS was missing
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export { app };
