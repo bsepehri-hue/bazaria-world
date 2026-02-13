2fa098bbec622e3f188d07a58cec193c9f711750
bsepehri-hue <bsepehri@gmail.com>
2026-02-12 22:57:10 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="light" style={{ colorScheme: "light" }}>
+   <html lang="en" suppressHydrationWarning>
       <body className="font-sans bg-slate-950 text-slate-50">
         <Providers>
           <WalletProvider>
