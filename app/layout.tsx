dae4b318155989e32e67f59e1617d22daab54325
bsepehri-hue <bsepehri@gmail.com>
2026-02-12 22:33:29 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <body className="font-sans bg-slate-950 text-slate-50">
-              <WalletProvider>
-            <Providers>
-              <ClientTopNavWrapper />
-              {children}
-            </Providers>
+        <Providers>
+          <WalletProvider>
+            <ClientTopNavWrapper />
+            {children}
           </WalletProvider>
-         </body>
+        </Providers>
+      </body>
     </html>
   );
 }
