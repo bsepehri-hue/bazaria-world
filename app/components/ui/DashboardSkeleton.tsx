8902c1e370deeac7af3ee60447f1dd6d8779addc
bsepehri-hue <bsepehri@gmail.com>
2026-02-13 19:57:51 -0800

    Create DashboardSkeleton.tsx

---
+++ app/components/ui/DashboardSkeleton.tsx
@@ -0,0 +1,9 @@
+export default function DashboardSkeleton() {
+  return (
+    <div className="p-6 animate-pulse space-y-4">
+      <div className="h-6 w-1/3 bg-gray-200 rounded"></div>
+      <div className="h-4 w-1/2 bg-gray-200 rounded"></div>
+      <div className="h-64 w-full bg-gray-200 rounded"></div>
+    </div>
+  );
+}
