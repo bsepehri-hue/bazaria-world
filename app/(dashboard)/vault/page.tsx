aec109fdb6ae7bb0834ee4b6fc0644fee0e906bb
bsepehri-hue <bsepehri@gmail.com>
2026-02-13 19:59:40 -0800

    Import DashboardSkeleton component in VaultDashboard

--- app/(dashboard)/vault/page.tsx
+++ app/(dashboard)/vault/page.tsx
@@ -13,6 +13,7 @@ import { useTheme } from "../../../hooks/useTheme";
 import { VaultDashboardData } from "@/lib/vault/types";
 import { mockVaultDashboardData } from "@/lib/vault/mockData";
 import { collection, getDocs } from "firebase/firestore";
+import DashboardSkeleton from "../../components/ui/DashboardSkeleton";
 
 export default function VaultDashboard() {
   const { isDark } = useTheme();
