8126cb4fa43dcd1e319bf5a41c41a0a0a7d813b9
bsepehri-hue <bsepehri@gmail.com>
2026-02-15 17:57:41 -0800

    Create CategoryIcons.ts

---
+++ components/icons/CategoryIcons.ts
@@ -0,0 +1,15 @@
+import { Frame } from "lucide-react";
+import { Ionicons } from "@expo/vector-icons";
+
+
+
+export const CategoryIcons = {
+  art: Frame,
+  pets: PawPrint,
+  cars: Car,
+  homes: Home,
+  rentals: Building,
+  rooms: DoorOpen,
+  general: Grid,
+  services: Wrench,
+};
