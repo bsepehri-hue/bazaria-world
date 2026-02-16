c22d9ac6f23af509adc44908ad4229f10870d83d
bsepehri-hue <bsepehri@gmail.com>
2026-02-15 18:01:19 -0800

    Update CategoryIcon.tsx

--- components/category/CategoryIcon.tsx
+++ components/category/CategoryIcon.tsx
@@ -1,1 +1,26 @@
-<Ionicons name="image-outline" size={24} color={color} />
+import { Ionicons } from "@expo/vector-icons";
+
+export default function CategoryIcon({ category, size = 24, color = "black" }) {
+  switch (category) {
+    case "art":
+      return <Ionicons name="image-outline" size={size} color={color} />;
+
+    case "pets":
+      return <Ionicons name="paw-outline" size={size} color={color} />;
+
+    case "cars":
+      return <Ionicons name="car-outline" size={size} color={color} />;
+
+    case "homes":
+      return <Ionicons name="home-outline" size={size} color={color} />;
+
+    case "services":
+      return <Ionicons name="construct-outline" size={size} color={color} />;
+
+    case "general":
+      return <Ionicons name="grid-outline" size={size} color={color} />;
+
+    default:
+      return <Ionicons name="ellipse-outline" size={size} color={color} />;
+  }
+}
