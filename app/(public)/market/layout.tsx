2782b28354f4371bbb70a4e23a24fe90186df968
bsepehri-hue <bsepehri@gmail.com>
2026-02-15 20:40:05 -0800

    Update layout.tsx

--- app/(public)/market/layout.tsx
+++ app/(public)/market/layout.tsx
@@ -1,12 +1,10 @@
-import TopNav from "@/app/components/ui/TopNav";
-
 export default function MarketLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <TopNav />
       <main>{children}</main>
     </div>
   );
 }
+
 
 
