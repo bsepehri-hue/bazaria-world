83579f1500bab7935d9841df59b3bbb1a1628fc7
bsepehri-hue <bsepehri@gmail.com>
2026-02-15 17:40:55 -0800

    Create specs.ts

---
+++ lib/categories/art/specs.ts
@@ -0,0 +1,30 @@
+export const ArtSpecs = [
+  {
+    key: "medium",
+    label: "Medium",
+  },
+  {
+    key: "materials",
+    label: "Materials",
+  },
+  {
+    key: "dimensions",
+    label: "Dimensions",
+  },
+  {
+    key: "yearCreated",
+    label: "Year Created",
+  },
+  {
+    key: "style",
+    label: "Style",
+  },
+  {
+    key: "condition",
+    label: "Condition",
+  },
+  {
+    key: "artist",
+    label: "Artist",
+  },
+];
