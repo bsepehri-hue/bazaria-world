9d660b8e59e16d1e7d9a4dd48cd08fd27cb18c40
bsepehri-hue <bsepehri@gmail.com>
2026-02-19 13:04:49 -0800

    Update page.tsx

--- app/pets/page.tsx
+++ app/pets/page.tsx
@@ -2,7 +2,7 @@ import ListingGrid from "@/components/listings/ListingGrid";
 import { getListingsByCategory } from "@/lib/listings";
 
 export default async function CategoryPage() {
-  const listings = await getListingsByCategory("CATEGORY_NAME");
+  const listings = await getListingsByCategory("pets");
   return <ListingGrid listings={listings} />;
 }
 
