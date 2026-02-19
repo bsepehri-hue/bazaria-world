a3375e92a6e7764a096d5d30678555ac143c3ee7
bsepehri-hue <bsepehri@gmail.com>
2026-02-19 13:30:38 -0800

    Update page.tsx

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,3 +1,6 @@
+import ListingGrid from "@/components/listings/ListingGrid";
+import { getListingsByCategory } from "@/lib/listings";
+
 export default async function RoomsPage() {
   const listings = await getListingsByCategory("rooms");
   return <ListingGrid listings={listings} />;
