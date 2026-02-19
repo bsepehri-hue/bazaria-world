799a7b56b2fa11ec3d09cae0937346a6cbed5e53
bsepehri-hue <bsepehri@gmail.com>
2026-02-19 12:59:31 -0800

    Update page.tsx

--- app/land/page.tsx
+++ app/land/page.tsx
@@ -1,1 +1,7 @@
+import ListingGrid from "@/components/listings/ListingGrid";
+import { getListingsByCategory } from "@/lib/listings";
 
+export default async function LandPage() {
+  const listings = await getListingsByCategory("land");
+  return <ListingGrid listings={listings} />;
+}
