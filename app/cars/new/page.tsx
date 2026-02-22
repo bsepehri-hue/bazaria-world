c785c22130e395c55abea200b6ea99c9402d71b7
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 14:47:41 -0800

    Update page.tsx

--- app/cars/new/page.tsx
+++ app/cars/new/page.tsx
@@ -1,1 +1,9 @@
+"use client";
+
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewCarListingPage() {
+  return <CreateListingForm category="cars" />;
+}
+
 
