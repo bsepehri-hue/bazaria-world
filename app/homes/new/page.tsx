4cad5c418f2cf0a49f7e0d3ec245e29a10e0f141
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 14:59:15 -0800

    Update page.tsx

--- app/homes/new/page.tsx
+++ app/homes/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewHomesListingPage() {
+  return <CreateListingForm category="homes" />;
+}
