c258da88dc9bbbb3a2168827086ac4350605133c
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 15:07:38 -0800

    Update page.tsx

--- app/rentals/new/page.tsx
+++ app/rentals/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewRentalListingPage() {
+  return <CreateListingForm category="rentals" />;
+}
