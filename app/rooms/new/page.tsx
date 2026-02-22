4ea99ca1d94e04c70f5cf0949e46ab41529a7900
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 15:09:39 -0800

    Update page.tsx

--- app/rooms/new/page.tsx
+++ app/rooms/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewRoomListingPage() {
+  return <CreateListingForm category="rooms" />;
+}
