5eda4acab05b6a8896ebe89b6390594f88665a91
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 15:02:56 -0800

    Update page.tsx

--- app/motorcycles/new/page.tsx
+++ app/motorcycles/new/page.tsx
@@ -1,2 +1,8 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewMotorcycleListingPage() {
+  return <CreateListingForm category="motorcycles" />;
+}
 
