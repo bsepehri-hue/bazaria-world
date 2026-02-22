7ada0014772cb8c913c685723ff0902d2f21f422
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 15:01:51 -0800

    Update page.tsx

--- app/land/[listingId]/page.tsx
+++ app/land/[listingId]/page.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import LandDetails from "@/components/listings/categories/land/LandDetails";
+
+export default function LandListingDetailsPage({
+  params,
+}: {
+  params: { listingId: string };
+}) {
+  return <LandDetails listingId={params.listingId} />;
+}
