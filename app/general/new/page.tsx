4adcdf686989d3c60fb8ee32f2396014a2ca497d
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 14:50:52 -0800

    Update page.tsx

--- app/general/new/page.tsx
+++ app/general/new/page.tsx
@@ -1,1 +1,9 @@
+"use client";
+
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewGeneralListingPage() {
+  return <CreateListingForm category="general" />;
+}
+
 
