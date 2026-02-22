2d746ac02971ec60c4e482440a5f1e4a2c5f5057
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 15:01:29 -0800

    Update page.tsx

--- app/land/new/page.tsx
+++ app/land/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewLandListingPage() {
+  return <CreateListingForm category="land" />;
+}
