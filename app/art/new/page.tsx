9a6103065f5d46495d6715322d55c65dd305c1a9
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 14:38:03 -0800

    Update page.tsx

--- app/art/new/page.tsx
+++ app/art/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewArtListingPage() {
+  return <CreateListingForm category="art" />;
+}
