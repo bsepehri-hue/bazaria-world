664e5902a119b0e50a329cae385c6f0f98eab3b9
bsepehri-hue <bsepehri@gmail.com>
2026-02-23 11:12:30 -0800

    Update page.tsx

--- app/timeshare/page.tsx
+++ app/timeshare/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import TimeshareListing from "@/components/listings/categories/timeshare/TimeshareListing";
-
 export default function TimesharePage() {
-  return <TimeshareListing />;
+  return <div>Timeshare page</div>;
 }
-
