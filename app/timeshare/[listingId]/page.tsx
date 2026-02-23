bbdf733c2090128c0f970e656c0d5a5dc85252a6
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 17:29:48 -0800

    Update page.tsx

--- app/timeshare/[listingId]/page.tsx
+++ app/timeshare/[listingId]/page.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import TimeshareDetails from "@/components/listings/categories/timeshare/TimeshareDetails";
+
+export default function TimeshareListingDetailsPage({
+  params,
+}: {
+  params: { listingId: string };
+}) {
+  return <TimeshareDetails listingId={params.listingId} />;
+}
