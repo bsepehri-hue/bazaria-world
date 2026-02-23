83ce88ea26cebdd305e55c642e6af3d1f5b1ab90
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 18:15:10 -0800

    Update page.tsx

--- app/cars/[listingId]/page.tsx
+++ app/cars/[listingId]/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
+import { use } from "react";
 import CarDetails from "@/components/listings/categories/cars/CarDetails";
 
-export default function CarListingDetailsPage({
-  params,
-}: {
-  params: { listingId: string };
-}) {
-  return <CarDetails listingId={params.listingId} />;
-}
+export default function CarListingDetailsPage({ params }: { params: Promise<{ listingId: string }> }) {
+  const { listingId } = use(params);
 
+  return <CarDetails listingId={listingId} />;
+}
