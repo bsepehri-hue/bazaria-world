0695f674e2e65da372f77ee1cb6e4c0eb908ca66
bsepehri-hue <bsepehri@gmail.com>
2026-02-23 06:30:38 -0800

    Update index.ts

--- lib/categories/index.ts
+++ lib/categories/index.ts
@@ -1,2 +1,32 @@
-export * as pets from "./pets";
-export * as art from "./art";
+import * as pets from "./pets";
+import * as art from "./art";
+
+export const MARKET_CATEGORIES = [
+  {
+    id: "cars",
+    label: "Cars",
+    icon: "🚗",
+  },
+  {
+    id: "trucks",
+    label: "Trucks",
+    icon: "🚚",
+  },
+  {
+    id: "rvs",
+    label: "RVs",
+    icon: "🚐",
+  },
+  {
+    id: "pets",
+    label: "Pets",
+    icon: "🐾",
+    ...pets,
+  },
+  {
+    id: "art",
+    label: "Art",
+    icon: "🎨",
+    ...art,
+  },
+];
