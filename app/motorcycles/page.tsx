f3ef26e87b62cc00e96f04fa75541757862a826c
bsepehri-hue <bsepehri@gmail.com>
2026-02-23 11:27:57 -0800

    Update page.tsx

--- app/motorcycles/page.tsx
+++ app/motorcycles/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import MotorcyclesListing from "@/components/listings/categories/motorcycles/MotorcyclesListing";
-
 export default function MotorcyclesPage() {
-  return <MotorcyclesListing />;
+  return <div>Motorcycles page</div>;
 }
