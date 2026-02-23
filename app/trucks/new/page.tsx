8a81f9fb9a9d5210cf20101462cabede472d4008
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 17:30:55 -0800

    Update page.tsx

--- app/trucks/new/page.tsx
+++ app/trucks/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewTruckListingPage() {
+  return <CreateListingForm category="trucks" />;
+}
