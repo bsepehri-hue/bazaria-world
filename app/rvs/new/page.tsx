aa350cb45d85a141706d900d924f4855fa536c76
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 17:24:56 -0800

    Update page.tsx

--- app/rvs/new/page.tsx
+++ app/rvs/new/page.tsx
@@ -1,1 +1,7 @@
+"use client";
 
+import CreateListingForm from "@/components/listings/CreateListingForm";
+
+export default function NewRvListingPage() {
+  return <CreateListingForm category="rvs" />;
+}
