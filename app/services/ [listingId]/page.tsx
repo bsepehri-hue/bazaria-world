ab20930bb3d6d0debce893d3f6749f4c9290adaf
bsepehri-hue <bsepehri@gmail.com>
2026-02-22 17:28:23 -0800

    Update page.tsx

--- app/services/ [listingId]/page.tsx
+++ app/services/ [listingId]/page.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import ServicesDetails from "@/components/listings/categories/services/ServicesDetails";
+
+export default function ServiceListingDetailsPage({
+  params,
+}: {
+  params: { listingId: string };
+}) {
+  return <ServicesDetails listingId={params.listingId} />;
+}
