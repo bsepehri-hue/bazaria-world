1c097b29a150ecb8343a15b02a5bd34ade3aab62
bsepehri-hue <bsepehri@gmail.com>
2026-02-24 15:53:46 -0800

    Update layout.tsx

--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -1,10 +1,10 @@
-import CategoryButtons from "../components/CategoryButtons";
+import CategoryMenu from "./CategoryMenu";
 
-export default function MarketLayout({ children }) {
+export default function MarketLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <CategoryButtons />
-      {children}
+      <CategoryMenu />
+      <main>{children}</main>
     </div>
   );
 }
