9f616b7d410b68c64f08705290b279eb398a37f9
bsepehri-hue <bsepehri@gmail.com>
2026-02-24 15:29:32 -0800

    Update layout.tsx

--- app/(public)/market/layout.tsx
+++ app/(public)/market/layout.tsx
@@ -1,10 +1,10 @@
+import CategoryMenu from "./CategoryMenu";
+
 export default function MarketLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="max-w-6xl mx-auto p-4">
+      <CategoryMenu />
       <main>{children}</main>
     </div>
   );
 }
-
-
-
