35eb96110d6c2ee624cf52fd7498606e51e2e2c6
bsepehri-hue <bsepehri@gmail.com>
2026-02-24 12:17:33 -0800

    Create page.tsx

---
+++ app/market/page.tsx
@@ -0,0 +1,10 @@
+import CategoryButtons from "@/components/CategoryButtons";
+
+export default function MarketPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Marketplace</h1>
+      <CategoryButtons />
+    </div>
+  );
+}
