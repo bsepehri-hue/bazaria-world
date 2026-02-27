b1f3140ab1c3e02c8f301232457b69a7c7d6980a
bsepehri-hue <bsepehri@gmail.com>
2026-02-27 07:34:50 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,28 @@ import ClientTopNavWrapper from "./components/ui/ClientTopNavWrapper";
 import { WalletProvider } from "./context/WalletContext";
 import { Providers } from "./providers";
 import GlobalCategoryMenu from "./components/GlobalCategoryMenu";
+import AuthProvider from "./providers/AuthProvider";
+
+
+
+import AuthProvider from "./providers/AuthProvider";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="font-sans bg-slate-950 text-slate-50">
-        <Providers>
-          <WalletProvider>
-            <ClientTopNavWrapper />
+        <AuthProvider>
+          <Providers>
+            <WalletProvider>
+              <ClientTopNavWrapper />
+              {children}
+            </WalletProvider>
+          </Providers>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
 
             {/* ⭐ Add the menu right here */}
             <GlobalCategoryMenu />
