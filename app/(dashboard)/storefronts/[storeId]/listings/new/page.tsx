382f42e3ff3a3615e67dcca8236072672c9a5382
bsepehri-hue <bsepehri@gmail.com>
2026-02-27 06:28:42 -0800

    Update page.tsx

--- app/(dashboard)/storefronts/[storeId]/listings/new/page.tsx
+++ app/(dashboard)/storefronts/[storeId]/listings/new/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+alert("handleFile fired");
+
 import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { addDoc, collection } from "firebase/firestore";
