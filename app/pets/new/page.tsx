ff6e07cf48ea8e5c31dc8b479332be659f5f657d
bsepehri-hue <bsepehri@gmail.com>
2026-02-27 07:18:45 -0800

    Update page.tsx

--- app/pets/new/page.tsx
+++ app/pets/new/page.tsx
@@ -3,6 +3,8 @@
 import { PetsForm } from "@/lib/categories/pets/form";
 import BaseListingForm from "@/components/listings/BaseListingForm";
 
+console.log("AUTH:", auth.currentUser);
+
 export default function NewPetListingPage() {
   return <BaseListingForm category="pets" form={PetsForm} />;
 }
