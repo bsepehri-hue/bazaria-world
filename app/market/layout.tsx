bc7a471c7dc80b95bca4aeeb82552238de7b19fc
bsepehri-hue <bsepehri@gmail.com>
2026-02-27 19:29:50 -0800

    Update layout.tsx

--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -2,9 +2,17 @@ import CategoryMenu from "./CategoryMenu";
 
 export default function MarketLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-6xl mx-auto p-4">
-      <CategoryMenu />
-      <main>{children}</main>
+    <div className="max-w-6xl mx-auto p-4 flex gap-6">
+      
+      {/* Left sidebar */}
+      <div className="w-64 shrink-0">
+        <CategoryMenu />
+      </div>
+
+      {/* Right content area */}
+      <main className="flex-1">
+        {children}
+      </main>
     </div>
   );
 }
