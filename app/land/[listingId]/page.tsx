367d0f2cbac1bf478f1d1675bce6103f850dc71f
bsepehri-hue <bsepehri@gmail.com>
2026-02-27 19:52:07 -0800

    Update page.tsx

--- app/land/[listingId]/page.tsx
+++ app/land/[listingId]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import LandDetails from "@/components/listings/categories/land/LandDetails";
+import { use } from "react";
+import ListingDetails from "@/components/listings/ListingDetails";
 
 export default function LandListingDetailsPage({
   params,
 }: {
-  params: { listingId: string };
+  params: Promise<{ listingId: string }>;
 }) {
-  return <LandDetails listingId={params.listingId} />;
+  const { listingId } = use(params);
+
+  return <ListingDetails category="Land" listingId={listingId} />;
 }
