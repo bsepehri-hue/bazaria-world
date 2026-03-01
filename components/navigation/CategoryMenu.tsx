8866acfc8ac28634ddc31c41ddbc8083460d216c
bsepehri-hue <bsepehri@gmail.com>
2026-02-28 19:46:20 -0800

    Update CategoryMenu.tsx

--- components/navigation/CategoryMenu.tsx
+++ components/navigation/CategoryMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { CategoryIcons } from "@/components/IconSet";
+import { CategoryIcons } from "../IconSet";
 
 const categories = [
   { id: "cars", label: "Cars" },
