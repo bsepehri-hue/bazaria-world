0c11f3b531bab4755f2af2632e9b1052dbbb4061
bsepehri-hue <bsepehri@gmail.com>
2026-03-02 08:12:46 -0800

    Update layout.tsx

--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -2,7 +2,7 @@ import CategoryMenu from "@/components/navigation/CategoryMenu";
 
 export default function MarketLayout({ children }) {
   return (
-   <div className="max-w-6xl mx-auto p-4 mt-20 relative z-0">
+   <div className="max-w-6xl mx-auto p-4 mt-20 flex flex-col">
       <div style={{ background: "red", height: "4px" }}></div>
 
       <CategoryMenu />
