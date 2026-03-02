3d84423faa31a6ceae653b68493b1717f4795a1e
bsepehri-hue <bsepehri@gmail.com>
2026-03-02 06:39:38 -0800

    Update layout.tsx

--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -2,10 +2,9 @@ import CategoryMenu from "@/components/navigation/CategoryMenu";
 
 export default function MarketLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-6xl mx-auto p-4">
+    <div className="max-w-6xl mx-auto p-4 mt-20">
       <div style={{ background: "red", height: "4px" }}></div>
 
-      {/* Marketplace-only menu */}
       <CategoryMenu />
 
       <main className="mt-6">
