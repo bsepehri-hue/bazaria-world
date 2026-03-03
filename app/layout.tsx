30d58a263fafa1c3b2640483d5f83914e4f6d804
bsepehri-hue <bsepehri@gmail.com>
2026-03-03 10:26:10 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
             <WalletProvider>
 
              <ClientTopNavWrapper>
-  <CategoryMenu />
+  
 </ClientTopNavWrapper>
 
 <div className="pt-[152px]">
