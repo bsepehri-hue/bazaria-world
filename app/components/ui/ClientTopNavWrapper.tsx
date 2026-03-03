61b4f95f4c3f7bd23383a29b7b135e753430c439
bsepehri-hue <bsepehri@gmail.com>
2026-03-03 10:00:52 -0800

    Update ClientTopNavWrapper.tsx

--- app/components/ui/ClientTopNavWrapper.tsx
+++ app/components/ui/ClientTopNavWrapper.tsx
@@ -1,9 +1,9 @@
-// NO "use client"
+"use client";
 import ClientTopNav from "@/app/components/ui/ClientTopNav";
 
 export default function ClientTopNavWrapper({ children }) {
   return (
-    <div className="relative z-[9999]">
+    <div className="fixed top-0 left-0 w-full z-[9999] bg-slate-950">
       <ClientTopNav />
       {children}
     </div>
