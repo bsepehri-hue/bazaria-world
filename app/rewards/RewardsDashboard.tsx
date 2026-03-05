458eb00b1120801c913a1946e35efb339c14d53f
bsepehri-hue <bsepehri@gmail.com>
2026-03-05 14:14:04 -0800

    Create RewardsDashboard.tsx

---
+++ app/rewards/RewardsDashboard.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+export default function RewardsDashboard() {
+  return (
+    <div className="rewards-content">
+
+      {/* Storefronts */}
+      <section className="storefronts">
+        {/* ... */}
+      </section>
+
+      {/* Auctions */}
+      <section className="auctions">
+        {/* ... */}
+      </section>
+
+      {/* Payable */}
+      <section className="payable">
+        {/* ... */}
+      </section>
+
+      {/* Steward Profile */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+      {/* Blessing Grid */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+      {/* Active Auction */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+      {/* Create Listing */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+      {/* Activity Log */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+      {/* Steward Summary */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+      {/* Arca */}
+      <section className="card">
+        {/* ... */}
+      </section>
+
+    </div>
+  );
+}
