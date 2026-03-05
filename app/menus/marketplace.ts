e4d442a18bc7ff3707af2e5c2f45eec832e2af48
bsepehri-hue <bsepehri@gmail.com>
2026-03-05 12:30:27 -0800

    Create marketplace.ts

---
+++ app/menus/marketplace.ts
@@ -0,0 +1,11 @@
+import {
+  faStore,
+  faList,
+  faTags
+} from "@fortawesome/free-solid-svg-icons";
+
+export const marketplaceMenu = [
+  { label: "Home", icon: faStore, href: "/marketplace" },
+  { label: "Categories", icon: faList, href: "/marketplace/categories" },
+  { label: "Listings", icon: faTags, href: "/marketplace/listings" }
+];
