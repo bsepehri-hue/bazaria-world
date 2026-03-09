0c8815c33fb05c3e4b9ae56ef0db382582f3bd8a
bsepehri-hue <bsepehri@gmail.com>
2026-03-09 13:08:39 -0700

    Update layout.tsx

--- app/(internal)/market/layout.tsx
+++ app/(internal)/market/layout.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import AppFrame from "@/components/layout/AppFrame";
-
 export default function MarketLayout({ children }) {
-  return <AppFrame>{children}</AppFrame>;
+  return children;
 }
