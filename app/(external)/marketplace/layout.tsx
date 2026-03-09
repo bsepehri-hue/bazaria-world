59ffeb342cb7e0e52533266792a5c9297673e3c9
bsepehri-hue <bsepehri@gmail.com>
2026-03-09 13:09:07 -0700

    Update layout.tsx

--- app/(external)/marketplace/layout.tsx
+++ app/(external)/marketplace/layout.tsx
@@ -1,15 +1,3 @@
-"use client";
-
-import AppFrame from "@/components/layout/AppFrame";
-import CategoryMenu from "@/components/navigation/CategoryMenu";
-
 export default function MarketplaceLayout({ children }) {
-  return (
-    <AppFrame>
-      <CategoryMenu />
-      <div className="pt-[56px]">
-        {children}
-      </div>
-    </AppFrame>
-  );
+  return children;
 }
