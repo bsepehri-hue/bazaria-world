d749ec69e954f79b5b36b96c9e9386e791542778
bsepehri-hue <bsepehri@gmail.com>
2026-03-09 16:19:57 -0700

    Update TopNavContainer.tsx

--- app/components/ui/TopNavContainer.tsx
+++ app/components/ui/TopNavContainer.tsx
@@ -1,17 +1,16 @@
 "use client";
 
 export default function TopNavContainer({ children }) {
-  console.log("🔥 USING TOPNAV CONTAINER");
   return (
-   <div
-  className="
-    fixed top-0 left-0 right-0 z-50
-    bg-white dark:bg-neutral-900 shadow-sm
-    h-16
-    px-6
-    flex items-center
-  "
->
+    <div
+      className="
+        fixed top-0 left-0 right-0 z-50
+        bg-white dark:bg-neutral-900 shadow-sm
+        h-16
+        flex items-center
+        px-4
+      "
+    >
       {children}
     </div>
   );
