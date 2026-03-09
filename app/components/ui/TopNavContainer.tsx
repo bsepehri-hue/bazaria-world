715e4d0610630a37d333069c7039a61c1ac4a736
bsepehri-hue <bsepehri@gmail.com>
2026-03-09 13:54:44 -0700

    Update TopNavContainer.tsx

--- app/components/ui/TopNavContainer.tsx
+++ app/components/ui/TopNavContainer.tsx
@@ -3,9 +3,7 @@
 export default function TopNavContainer({ children }) {
   return (
     <div className="fixed top-0 left-0 right-0 z-50 bg-white dark:bg-neutral-900 shadow-sm">
-     <div className="w-full px-6">
-  {children}
-</div>
+      {children}
     </div>
   );
 }
