dc459719b57cf1fa2894c1fe24d39119d6fe3c50
bsepehri-hue <bsepehri@gmail.com>
2026-03-10 11:20:53 -0700

    Update AppFrame.tsx

--- components/layout/AppFrame.tsx
+++ components/layout/AppFrame.tsx
@@ -32,19 +32,21 @@ export default function AppFrame({ children }) {
   else if (path.startsWith("/payable")) menu = payableMenu;
   else if (path.startsWith("/rewards")) menu = rewardsMenu;
 
+  // Public pages bypass the frame
   if (path.startsWith("/auction-link") || path.startsWith("/public")) {
     return <>{children}</>;
   }
 
   return (
-   <div className="w-full min-h-screen flex flex-col overflow-hidden">
+    <div className="w-full min-h-screen flex flex-col overflow-hidden">
+
       {/* FIXED TOP NAV */}
       <TopNavContainer>
         <TopNav />
       </TopNavContainer>
 
-      {/* MAIN AREA */}
-      <div className="flex flex-row flex-1 pt-16 overflow-auto">
+      {/* EVERYTHING BELOW THE TOP NAV */}
+      <div className="flex flex-row flex-1 overflow-hidden mt-16">
 
         <aside className="sidebar-container h-full overflow-y-auto">
           <SidebarMenu menu={menu} />
