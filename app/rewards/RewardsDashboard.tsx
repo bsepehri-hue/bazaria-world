8478710a417ba1cc07304c40f3e7ab5bee5dd7e7
bsepehri-hue <bsepehri@gmail.com>
2026-03-10 06:20:23 -0700

    Update RewardsDashboard.tsx

--- app/rewards/RewardsDashboard.tsx
+++ app/rewards/RewardsDashboard.tsx
@@ -2,7 +2,7 @@
 
 export default function RewardsDashboard() {
   return (
-    <div className="rewards-content">
+    <div className="w-full h-auto p-6">
 
       {/* Storefronts */}
       <section className="storefronts">
