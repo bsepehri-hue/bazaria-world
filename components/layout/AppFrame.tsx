ea02f95554ce8a27d5c5e43ddf6ce99172486b5e
bsepehri-hue <bsepehri@gmail.com>
2026-03-10 14:08:00 -0700

    Update AppFrame.tsx

--- components/layout/AppFrame.tsx
+++ components/layout/AppFrame.tsx
@@ -1,9 +1,10 @@
+TypeScript
 "use client";
 
 import { usePathname } from "next/navigation";
 import SidebarMenu from "@/components/sidebar/SidebarMenu";
 import TopNav from "@/app/components/ui/TopNav";
-import TopNavContainer from "@/app/components/ui/TopNavContainer";
+// 🚨 We are completely removing the TopNavContainer import to kill the ghost CSS
 
 import { marketplaceMenu } from "@/menus/marketplace";
 import { storefrontsMenu } from "@/menus/storefronts";
@@ -38,21 +39,24 @@ export default function AppFrame({ children }) {
   }
 
   return (
-    <div className="w-full min-h-screen flex flex-col overflow-hidden">
+    // 1. h-screen locks the app to the window height so we can scroll the main content independently
+    <div className="w-full h-screen flex flex-col overflow-hidden bg-white">
 
-      {/* FIXED TOP NAV */}
-      <TopNavContainer>
+      {/* 2. FIXED TOP NAV - Replaced TopNavContainer with a pure Tailwind header */}
+      <header className="h-16 w-full flex-shrink-0 z-50">
         <TopNav />
-      </TopNavContainer>
+      </header>
 
       {/* EVERYTHING BELOW THE TOP NAV */}
-    <div className="flex flex-row flex-1 overflow-hidden">
+      <div className="flex flex-row flex-1 overflow-hidden">
 
-        <aside className="sidebar h-full overflow-y-auto">
+        {/* 3. SIDEBAR - Dropped '.sidebar'. w-64 gives fixed width, shrink-0 prevents it from getting crushed */}
+        <aside className="w-64 flex-shrink-0 h-full overflow-y-auto border-r border-gray-200">
           <SidebarMenu menu={menu} />
         </aside>
 
-        <main className="flex-1 overflow-y-auto p-6 pt-24">
+        {/* 4. MAIN CONTENT - flex-1 takes up the rest of the screen on the right */}
+        <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
           {children}
         </main>
 
