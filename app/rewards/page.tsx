a7ac629d9efae1f4d02bfca9af0a7157c041aebb
bsepehri-hue <bsepehri@gmail.com>
2026-03-10 13:02:31 -0700

    Update page.tsx

--- app/rewards/page.tsx
+++ app/rewards/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import AppFrame from "@/components/layout/AppFrame";
 import RewardsDashboard from "./RewardsDashboard";
 
 export default function RewardsPage() {
-  return (
-    <AppFrame>
-      <RewardsDashboard />
-    </AppFrame>
-  );
+  return <RewardsDashboard />;
 }
