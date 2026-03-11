0d99b6d135250f29c6b83d0a72cfe434b1542f62
bsepehri-hue <bsepehri@gmail.com>
2026-03-11 11:29:57 -0700

    Create MarketplaceCard.tsx

---
+++ app/market/MarketplaceCard.tsx
@@ -0,0 +1,16 @@
+// app/market/MarketplaceCard.tsx
+
+export default function MarketplaceCard() {
+  return (
+    <div className="marketplace-card">
+      {/* Image placeholder */}
+      <div className="card-image" />
+
+      {/* Content placeholder */}
+      <div className="card-content">
+        <div className="card-title" />
+        <div className="card-subtitle" />
+      </div>
+    </div>
+  );
+}
