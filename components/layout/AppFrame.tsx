55d2d5dedb66fc5cfc4c60fc2a3f5f78cbf0870f
bsepehri-hue <bsepehri@gmail.com>
2026-03-11 15:20:58 -0700

    Update AppFrame.tsx

--- components/layout/AppFrame.tsx
+++ components/layout/AppFrame.tsx
@@ -1,35 +1,11 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import SidebarMenu from "@/components/sidebar/SidebarMenu";
+import Sidebar from "@/components/layout/Sidebar";
 import TopNav from "@/app/components/ui/TopNav";
-
-import { marketplaceMenu } from "@/menus/marketplace";
-import { storefrontsMenu } from "@/menus/storefronts";
-import { auctionsMenu } from "@/menus/auctions";
-import { vaultMenu } from "@/menus/vault";
-import { adminMenu } from "@/menus/admin";
-import { messagesMenu } from "@/menus/messages";
-import { settingsMenu } from "@/menus/settings";
-import { payableMenu } from "@/menus/payable";
-import { rewardsMenu } from "@/menus/rewards";
-import { marketMenu } from "@/menus/market";
 
 export default function AppFrame({ children }) {
   const path = usePathname();
-
-  let menu = marketplaceMenu;
-
-  if (path.startsWith("/marketplace")) menu = marketplaceMenu;
-  else if (path.startsWith("/market")) menu = marketMenu;
-  else if (path.startsWith("/storefronts")) menu = storefrontsMenu;
-  else if (path.startsWith("/auctions")) menu = auctionsMenu;
-  else if (path.startsWith("/vault")) menu = vaultMenu;
-  else if (path.startsWith("/admin")) menu = adminMenu;
-  else if (path.startsWith("/messages")) menu = messagesMenu;
-  else if (path.startsWith("/settings")) menu = settingsMenu;
-  else if (path.startsWith("/payable")) menu = payableMenu;
-  else if (path.startsWith("/rewards")) menu = rewardsMenu;
 
   if (path.startsWith("/auction-link") || path.startsWith("/public")) {
     return <>{children}</>;
@@ -40,10 +16,12 @@ export default function AppFrame({ children }) {
       <header className="topnav">
         <TopNav />
       </header>
+
       <div className="page-body">
         <aside className="sidebar">
-          <SidebarMenu menu={menu} />
+          <Sidebar />
         </aside>
+
         <main className="page-content">
           {children}
         </main>
