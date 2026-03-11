1821148c08beab765d0f9bddbed6050be2fb88da
bsepehri-hue <bsepehri@gmail.com>
2026-03-11 15:26:23 -0700

    Update sidebarItems.ts

--- components/layout/sidebarItems.ts
+++ components/layout/sidebarItems.ts
@@ -1,5 +1,3 @@
-// components/layout/sidebarItems.ts
-
 import {
   FaStore,
   FaGift,
@@ -9,20 +7,18 @@ import {
   FaCog,
   FaEnvelope,
   FaBell,
-  FaHeadset, // ← Support icon (FA6)
+  FaHeadset, // Support icon
 } from "react-icons/fa6";
-
-
 
 export const sidebarItems = [
   { name: "Marketplace", href: "/market", icon: FaStore },
   { name: "Rewards", href: "/rewards", icon: FaGift },
   { name: "Vault", href: "/vault", icon: FaWallet },
-  { name: "Storefronts", href: "/storefronts", icon: FaShop },
+  { name: "Storefronts", href: "/storefronts", icon: FaStore }, // FIXED
   { name: "Auctions", href: "/auctions", icon: FaGavel },
   { name: "Admin", href: "/admin", icon: FaUserShield },
   { name: "Settings", href: "/settings", icon: FaCog },
   { name: "Messages", href: "/messages", icon: FaEnvelope },
   { name: "Notifications", href: "/notifications", icon: FaBell },
-  { name: "Support", href: "/support", icon: FaLifeRing },
+  { name: "Support", href: "/support", icon: FaHeadset }, // FIXED
 ];
