6458144f9f2c7d5c922f5f61b98557c1bb4e6417
bsepehri-hue <bsepehri@gmail.com>
2026-03-13 07:29:34 -0700

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -6,6 +6,15 @@ import { sidebarItems } from "./sidebarItems";
 export default function Sidebar() {
   return (
     <aside className="sidebar">
+
+      {/* SIDEBAR LOGO */}
+      <div className="flex flex-col items-center mt-4 mb-6">
+        <span className="text-white tracking-wide font-semibold text-lg">
+          BAZARIA
+        </span>
+        <div className="w-12 h-[2px] bg-amber-400 mt-1 rounded-full opacity-90"></div>
+      </div>
+
       <nav className="sidebar-menu">
         {sidebarItems.map((item) => (
           <Link
