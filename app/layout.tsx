1a7a0e3f2fbc925e4743358fdea4326187843ba2
bsepehri-hue <bsepehri@gmail.com>
2026-03-15 10:49:48 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import "./globals.css";
 import { AppProviders } from "./AppProviders";
-import TopNav from "./components/ui/TopNav";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="font-sans bg-slate-950 text-slate-50">
         <AppProviders>
-          <TopNav />
           {children}
         </AppProviders>
       </body>
