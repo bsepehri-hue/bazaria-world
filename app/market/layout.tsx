428c55db4e355a75c895de4ba4f75ea8c04d0581
bsepehri-hue <bsepehri@gmail.com>
2026-03-16 12:27:33 -0700

    Update layout.tsx

--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -5,7 +5,7 @@ import AppFrame from "../../components/layout/AppFrame";
 export default function MarketLayout({ children }) {
   return (
     <AppFrame>
-      <CategoryBar />
+    
       {children}
     </AppFrame>
   );
