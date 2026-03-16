e3af7c844f74116b4bdb7122f3ea5b19476bc687
bsepehri-hue <bsepehri@gmail.com>
2026-03-16 12:18:21 -0700

    Update layout.tsx

--- app/market/layout.tsx
+++ app/market/layout.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import AppFrame from "../../components/layout/AppFrame";
+import CategoryBar from "@/components/marketplace/CategoryBar";
 
 export default function MarketLayout({ children }) {
-  return <AppFrame>{children}</AppFrame>;
+  return (
+    <AppFrame>
+      <CategoryBar />
+      {children}
+    </AppFrame>
+  );
 }
