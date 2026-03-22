cdf61328fab247b22d56cd1009262eb857d0d265
bsepehri-hue <bsepehri@gmail.com>
2026-03-22 00:12:20 -0700

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -7,13 +7,16 @@ export default function Sidebar() {
   return (
   <aside className="bazaria-sidebar">
 
-  {/* SIDEBAR LOGO */}
- <div className="flex flex-col items-center mt-10 mb-8">
-  <span className="text-white tracking-wide font-semibold text-[17px]">
-    BAZARIA
-  </span>
-  <div className="w-10 h-[2px] bg-amber-400 mt-1 rounded-full opacity-100"></div>
-</div>
+  {/* --- SIDEBAR LOGO: REFINED BRANDING --- */}
+  <div className="flex flex-col items-start px-8 mt-10 mb-10"> 
+    {/* items-start aligns it to the left side of the sidebar padding */}
+    <span className="text-white tracking-[0.1em] font-black text-[24px] leading-none">
+      BAZARIA
+    </span>
+    
+    {/* The Amber Twist: A sharp, solid line that accents the brand */}
+    <div className="w-16 h-[3px] bg-[#FFBF00] mt-2 shadow-[0_0_10px_rgba(255,191,0,0.3)]"></div>
+  </div>
 
   <nav className="sidebar-menu">
     {sidebarItems.map((item) => (
