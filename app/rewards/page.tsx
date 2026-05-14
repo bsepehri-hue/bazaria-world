69c39d630497d09d10858f79d9d2863946e097bc
bsepehri-hue <bsepehri@gmail.com>
2026-05-14 09:28:59 -0700

    Update page.tsx

--- app/rewards/page.tsx
+++ app/rewards/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import React from "react";
-// 🎯 Use this specific pathing to force a clean break from the "ghost" component
-import * as DashboardCore from "./RewardsDashboard";
+import RewardsDashboard from "./RewardsDashboard";
 
 export default function RewardsPage() {
-  // We call the named export specifically from the module
-  return <DashboardCore.RewardsDashboard />;
+  return <RewardsDashboard />;
 }
