b636d7ee5fb26f04f0bd06c73441872aa3b316a3
bsepehri-hue <bsepehri@gmail.com>
2026-05-30 21:31:04 -0700

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+
 // 📡 IMPORT FIREBASE COMPLIANT BACKGROUND SCRIPTS
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
