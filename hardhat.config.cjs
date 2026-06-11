14e580939fc03fdc120344130a4db1991041baa8
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 13:01:38 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,18 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-ignition-ethers";
-import "dotenv/config";
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require("dotenv").config();
 
-const config: HardhatUserConfig = {
+module.exports = {
   solidity: "0.8.24",
   networks: {
     amoy: {
       url: "https://rpc-amoy.polygon.technology",
-      // Explicitly removed 'type' to avoid HHE15
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
       chainId: 80002,
     },
   },
 };
-
-export default config;
