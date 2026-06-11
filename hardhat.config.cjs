98b66f00a79e965538adb6ef6987e1c490fa8447
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 12:51:48 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,18 +1,14 @@
-# 1. Update the CJS file that is actually being used by Hardhat
-Set-Content -Path .\hardhat.config.cjs -Value @"
 require('dotenv').config();
+require('@nomicfoundation/hardhat-ignition-ethers'); // <--- This line is critical
 
 module.exports = {
   solidity: '0.8.24',
   networks: {
     amoy: {
       url: 'https://rpc-amoy.polygon.technology',
+      type: 'http',
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
       chainId: 80002,
     },
   },
 };
-"@
-
-# 2. Tell git to ignore this file so the 'pull' doesn't overwrite your fix again
-git update-index --assume-unchanged hardhat.config.cjs
