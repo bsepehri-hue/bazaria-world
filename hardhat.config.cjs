90abea3edfddff90230772171da78c2f8060199b
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 13:11:18 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     amoy: {
+      type: "http", // Required for Hardhat 3
       url: "https://rpc-amoy.polygon.technology",
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
       chainId: 80002,
