c6b8225746bcaac6cdd2344b73b12f1710093cd7
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 12:37:25 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 
 export default {
-  solidity: "0.8.24",
+  solidity: '0.8.24',
   networks: {
     amoy: {
-      url: "https://rpc-amoy.polygon.technology",
+      url: 'https://rpc-amoy.polygon.technology',
+      type: 'http', 
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
       chainId: 80002,
     },
