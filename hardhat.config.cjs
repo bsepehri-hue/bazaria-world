419217e0e678ccc6130641d563107a71290a00ff
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 13:13:29 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,3 +1,4 @@
+Set-Content -Path .\hardhat.config.js -Value @"
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition-ethers");
 require("dotenv").config();
@@ -6,10 +7,11 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     amoy: {
-      type: "http", // Required for Hardhat 3
+      type: "http",
       url: "https://rpc-amoy.polygon.technology",
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
       chainId: 80002,
     },
   },
 };
+"@
