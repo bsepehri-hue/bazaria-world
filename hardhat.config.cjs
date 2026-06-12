67f58cc7946aca2f962e338fe90e247103dbc0ac
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 17:58:36 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,13 +1,13 @@
 import "dotenv/config";
+import "@nomicfoundation/hardhat-ethers";
 import ignitionEthers from "@nomicfoundation/hardhat-ignition-ethers";
 
 export default {
   solidity: "0.8.24",
-  // In Hardhat 3, explicitly registering the plugin object
+  // Manually injecting the plugin instance into the HRE
   plugins: [ignitionEthers],
   networks: {
     amoy: {
-      type: 'http',
       url: "https://rpc-amoy.polygon.technology",
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
       chainId: 80002,
