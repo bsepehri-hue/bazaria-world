7d92179c9b8d7d452e2471da4c92773b36712f45
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 18:07:16 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,11 +1,8 @@
 import "dotenv/config";
-import ignitionEthers from "@nomicfoundation/hardhat-ignition-ethers";
+import "@nomicfoundation/hardhat-ignition-ethers";
 
 export default {
   solidity: "0.8.24",
-  // This array registration is mandatory in Hardhat 3 for plugins
-  // to correctly link their tasks to the CLI
-  plugins: [ignitionEthers],
   networks: {
     amoy: {
       url: "https://rpc-amoy.polygon.technology",
