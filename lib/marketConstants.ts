cc0969f657eaf7e0fa9da23d100ca1f3f01f8cba
bsepehri-hue <bsepehri@gmail.com>
2026-06-12 00:26:42 -0700

    Create marketConstants.ts

---
+++ lib/marketConstants.ts
@@ -0,0 +1,6 @@
+import BazariaMarketplaceABI from '../contracts/abis/BazariaMarketplace.json';
+
+export const DIGITAL_MARKET_CONFIG = {
+  address: "0x7c211077dBb177a4b2a551DA7CdC3D53b04Cbdb7",
+  abi: BazariaMarketplaceABI,
+};
