c47966e36970d43c8985d8425f39d0f53cd45875
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 17:54:19 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,9 +1,10 @@
-// File: hardhat.config.js
-import "@nomicfoundation/hardhat-ignition-ethers";
+import ignitionEthers from "@nomicfoundation/hardhat-ignition-ethers";
 import "dotenv/config";
 
 export default {
   solidity: "0.8.24",
+  // This array registration is mandatory for HRE to find the tasks
+  plugins: [ignitionEthers],
   networks: {
     amoy: {
       type: 'http',
