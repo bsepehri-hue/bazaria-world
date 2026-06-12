da291340606c774ab80fd92e259a136755f1c635
bsepehri-hue <bsepehri@gmail.com>
2026-06-11 18:03:05 -0700

    Update hardhat.config.cjs

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,10 +1,9 @@
 import "dotenv/config";
+import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-ignition-ethers";
 
 export default {
   solidity: "0.8.24",
-  // We use this to force the HRE to see the ignition tasks
-  plugins: ["@nomicfoundation/hardhat-ignition-ethers"],
   networks: {
     amoy: {
       url: "https://rpc-amoy.polygon.technology",
