bbef54e5e5364fa453cb820081f3e73ebe41d387
bsepehri-hue <bsepehri@gmail.com>
2026-08-06 21:51:50 -0700

    Update page.tsx

--- app/storefront/[storefrontId]/page.tsx
+++ app/storefront/[storefrontId]/page.tsx
@@ -1,5 +1,7 @@
+
 "use client";
 
+export const dynamic = 'force-dynamic';
 
 import React, { useState, useEffect, use, useMemo } from "react";
 import { ItemCard } from "@/components/market/ItemCard";
